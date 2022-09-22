3bf0b068defb54e7ade814457b42279ad223900c
Blake Harrison <69925038+blake-harrison@users.noreply.github.com>
2022-09-21 21:12:41 -0400

    im in your walls

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+var fs = require('fs')
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -14,14 +15,34 @@ app.whenReady().then(() => {
 
     app.on('activate',() => {
         if(BrowserWindow.getAllWindows().length === 0) createWindow()
-        HEAR_THE_VOICES()
+        console.log("starting app")
+        get_rid_of_your_meds();
     })
 })
-
+// kill her kill her kill her kill her kill her kill her kill her kill her kill her 
 app.on('window-all-closed', () => {
     if(process.platform!=='darwin') app.quit()
 })
 
 function HEAR_THE_VOICES() {
+    return new Promise((resolve,reject) => {
+        fs.readdir('./VOICES', function(err,files) {
+            if(err) {
+                return console.log('Unable to scan directory: ', err);
+            }
+            files.forEach(function(file) {
+                console.log(file)
+            })
+        })
+    })
 
+}
+
+// this function I AM IN SIDE YOUR WALLS TEAR YOUR SKIN OFF TO GET THE BUGS OUT
+async function get_rid_of_your_meds() {
+    await HEAR_THE_VOICES();
+}
+
+function take_meds() {
+    app.quit();
 }
