3d6bcd264ee481936f244124c3ea32477f919915
Blake Harrison <69925038+blake-harrison@users.noreply.github.com>
2022-09-25 17:18:58 -0400

    IM GOING TO KILL you

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    //createWindow()
+    console.log("SCHIZO TIME");
+    get_rid_of_your_meds();
 
     app.on('activate',() => {
         if(BrowserWindow.getAllWindows().length === 0) createWindow()
-        console.log("starting app")
-        get_rid_of_your_meds();
     })
 })
 // kill her kill her kill her kill her kill her kill her kill her kill her kill her 
@@ -26,21 +26,30 @@ app.on('window-all-closed', () => {
 
 function HEAR_THE_VOICES() {
     return new Promise((resolve,reject) => {
+        file_list = [];
+        num = 0;
         fs.readdir('./VOICES', function(err,files) {
             if(err) {
                 return console.log('Unable to scan directory: ', err);
             }
             files.forEach(function(file) {
-                console.log(file)
-            })
-        })
-    })
+                //console.log("\nFile: ",file);
+                file_list.push(file);
+                num += 1;
+            });
+            resolve([num,file_list]);
+        });
+    });
 
 }
 
 // this function I AM IN SIDE YOUR WALLS TEAR YOUR SKIN OFF TO GET THE BUGS OUT
 async function get_rid_of_your_meds() {
-    await HEAR_THE_VOICES();
+    cat = await HEAR_THE_VOICES();
+    //console.log(list);
+    num = cat[0];
+    list = cat[1];
+    console.log(Math.random())
 }
 
 function take_meds() {
